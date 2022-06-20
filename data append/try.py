51d6993db3831cd7dd7ad4b9a2873d86b70c7fe2
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-06-20 10:36:25 +0530

    Update try.py

--- data append/try.py
+++ data append/try.py
@@ -1,11 +1,13 @@
 from csv import writer
 import streamlit as st  
 
-
-take_input = st.text_input("Write your Name") 
-take_new_input = st.number_input("Enter your Age", value=23)
+st.header("Append Data in CSV File")
+name = st.text_input("Enter your Name") 
+age = st.number_input("Enter your Age", value=23)
+rating = st.slider("Rating of Session", min_value=0, max_value=5, value=0)
+remark = st.text_area("Remarks",value="Remark is None")
 if st.button("Submit Information"):
-    list_data=[take_input,take_new_input]
+    list_data=[name,age,rating,remark]
   
     with open('data.csv', 'a', newline='') as f_object:  
         writer_object = writer(f_object)
